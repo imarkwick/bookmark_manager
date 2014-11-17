65509bd23611b3a184535545632d8e0a3c8368f9
imarkwick <izzymarkwick@gmail.com>
2014-11-17 23:07:00 +0000

    Adding links and tags

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -29,6 +29,12 @@ class MyApp < Sinatra::Base
   	redirect to('/')
   end
 
+  get '/tags/:text' do
+    tag = Tag.first(:text => params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/list_all_links_spec.rb
+++ spec/features/list_all_links_spec.rb
@@ -1,12 +1,29 @@
 require 'spec_helper'
 	
 feature "User browses the list of links" do
+
+
 	before(:each) {
 	Link.create(:url => "http://www.makersacademy.com",
-							:title => "Makers Academy")
+							:title => "Makers Academy",
+							:tags => [Tag.first_or_create(:text => 'education')])
+	Link.create(:url => "http://www.google.com",
+							:title => "Google"
+							:tags => [Tage.first_or_create(:text => 'search')])
+	Link.create(:url => "http://www.bing.com",
+							:title => "Bing",
+							:tags => [Tage.forst_or_create(:text => 'search')])
+	Link.create(:url => "http://www.code.org",
+							:title => "Code.ord",
+							:tags => [Tage.first_or_create(:text => 'education')])
 	}
-		scenario "when opening the home page" do
-			visit '/'
-			expect(page).to have_content("Makers Academy")
-		end
+	end
+
+	scenario "filtered by a tag" do
+		visit '/tage/search'
+		expect(page).not_to have_content("Makers Academy")
+		expect(page).not_to have_content("Code.org")
+		expect(page).to have_content("Google")
+		expect(page).to have_content("Bing")
+	end
 end
