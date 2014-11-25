45fe4592f25b7f33cc5a2df339dd22913da48df5
imarkwick <izzymarkwick@gmail.com>
2014-11-25 09:39:46 +0000

    putting back in the forgot password button

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -1,11 +1,11 @@
+  get '/links/new' do
+    erb :"links/new"
+  end
+
   post '/links' do
   	url = params["url"]
   	title = params["title"]
     tags = params["tags"].split(" ").map{ |tag| Tag.first_or_create(:text => tag) }
   	Link.create(:url => url, :title => title, :tags => tags)
   	redirect to('/')
-  end
-
-  get '/links/new' do
-  	erb :"links/new"
   end
